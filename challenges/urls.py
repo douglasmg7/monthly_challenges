08543e5ffffe73bd077657a91a44ec8decf9c9d3
douglasmg7 <douglasmg7@gmail.com>
2026-06-30 09:55:29 -0300

    reverse url name

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('january', views.index),
-    path('february', views.february),
+    path('', views.index, name='month-challenge-index'),
+    path('index', views.index),
     path('<int:month>', views.monthly_challenge_by_number),
-    path('<str:month>', views.monthly_challenge),
+    path('<str:month>', views.monthly_challenge, name='month-challenge'),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
         'january': 'Hello January',
@@ -9,7 +10,7 @@ monthly_challenges = {
         }
 
 def index(request):
-    return HttpResponse("Hello January!")
+    return HttpResponse("Hello index!")
 
 def february(request):
     return HttpResponse("Hello February!")
@@ -17,15 +18,19 @@ def february(request):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        return HttpResponse(f'<h1>{challenge_text}</h1>')
     except:
         return HttpResponseNotFound('Month not supported')
 
 #  Month by number
 def monthly_challenge_by_number(request, month):
+    # Starting from 1.
+    month = month - 1
+    if month > 12 or month < 1:
+        return HttpResponseNotFound('Month not supported')
     try:
         str_month = list(monthly_challenges.keys())[month]
-        HttpResponseRedirect('/challenges/' + str_month)
-        #  return HttpResponse(str_month)
+        return HttpResponseRedirect(reverse('month-challenge', args=[str_month]))
+        #  return HttpResponseRedirect('/challenges/' + str_month)
     except:
         return HttpResponseNotFound('Month not supported')
