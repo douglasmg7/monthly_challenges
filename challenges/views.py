9c641bc6c52ab49849cb453d2ba5e550e274219a
douglasmg7 <douglasmg7@gmail.com>
2026-06-30 21:37:06 -0300

    templates

--- challenges/views.py
+++ challenges/views.py
@@ -6,16 +6,37 @@ monthly_challenges = {
         'january': 'Hello January',
         'february': 'Hello February',
         'march': 'Hello March',
-        'april': 'Hello April'
+        'april': 'Hello April',
+        'may': 'Hello May',
+        'june': 'Hello June',
+        'july': 'Hello July',
+        'august': 'Hello August',
+        'september': 'Hello September',
+        'october': 'Hello October',
+        'november': 'Hello November',
+        'december': None
         }
 
 def index(request):
-    return HttpResponse("Hello index!")
+    return render(request, 'challenges/index.html', {'months': list(monthly_challenges.keys())})
+
+def index_old(request):
+    li = [f"<li><a href='{reverse("month-challenge", args=[month])}'>{month.capitalize()}</a></li>" 
+          for month in list(monthly_challenges.keys())]
+
+    return HttpResponse('<ul>\n\t' + '\n\t'.join(li) + '\n</ul>')
 
 def february(request):
     return HttpResponse("Hello February!")
 
 def monthly_challenge(request, month):
+    try:
+        #  challenge_text = monthly_challenges[month]
+        return render(request, 'challenges/challenge.html', {'month': month, 'message': monthly_challenges[month]})
+    except:
+        return HttpResponseNotFound('Month not supported yet')
+
+def monthly_challenge_old(request, month):
     try:
         challenge_text = monthly_challenges[month]
         return HttpResponse(f'<h1>{challenge_text}</h1>')
